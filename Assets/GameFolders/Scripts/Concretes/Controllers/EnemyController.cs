a81e9b783d62928a51b3a3782ec5ce75107cbb7e
Halil İbrahim <77546808+yilmazhalilibr@users.noreply.github.com>
2023-07-28 12:57:59 +0300

    Enemy prefab , ia navigation

---
+++ Assets/GameFolders/Scripts/Concretes/Controllers/EnemyController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameProject3.Controllers
+{
+    public class EnemyController : MonoBehaviour
+    {
+
+    }
+
+}
